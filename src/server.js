b565eabf2d03623a2ff360aef6dd37180fe376c0
moonbs95 <moonbs0318@gmail.com>
2023-06-10 00:10:13 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,46 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import usersRouter from "./routers/usersRouter";
+import videosRouter from "./routers/videosRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+app.use("/", globalRouter);
+app.use("/users", usersRouter);
+app.use("/videos", videosRouter);
 
-const securityLogger = (req, res, next) => {
-    if (req.protocol === "https") {
-      console.log("Secure");
-    } else {
-      console.log("Insecure");
-    }
-    next();
-  };
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Welcome");
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.send("This is HP");
-};
-
-const handleProtect = (req, res) => {
-    return res.send("Private Lounge for VIP");
-};
-
-app.use(loggerMiddleware);
-app.use(securityLogger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtect);
-
-const handleListening = () => console.log(`server listening on port https://localhost:${PORT}`);
+const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
