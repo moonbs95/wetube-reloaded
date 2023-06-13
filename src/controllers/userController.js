13f2ad28b1aac10cca53f85214ee8f7929e12b21
moonbs95 <moonbs0318@gmail.com>
2023-06-13 20:22:13 +0900

    update controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,6 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("login");
+export const remove = (req, res) => res.send("remove");
+export const edit = (req, res) => res.send("edit");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
-export const watch = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit")
 export const search = (req, res) => res.send("search");
 export const uploadVideo = (req, res) => res.send("upload video");
